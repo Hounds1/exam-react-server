8f4bb46e8ba1cd289d9327448df6e5872c0f215f
Hounds1 <zoodisx@gmail.com>
2025-09-24 18:51:42 +0900

    git init

---
+++ src/test/java/exam/react/io/IoApplicationTests.java
@@ -0,0 +1,13 @@
+package exam.react.io;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
